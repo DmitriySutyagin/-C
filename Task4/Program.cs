622cf2ed16e60b7d58d721bd3deb97942240c82d
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-08 10:37:42 +0300

    Решение четвертой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Enter the first number");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third number");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number;
+if (number > number2)
+{
+    max = number;
+}
+else
+{
+    max = number2;
+    if (number2 > number3);
+    else
+    {
+        max = number3;
+    }
+
+}
+Console.WriteLine(max);
