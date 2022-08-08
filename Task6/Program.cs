fe131fc77365959fdd3c7a16ac254151b54d3462
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-08 11:32:25 +0300

    Решение шестой задачи

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Yes") ;
+}
+else
+{
+    Console.WriteLine("No");
+}
