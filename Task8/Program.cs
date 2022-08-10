0ebfdc1185287dbd3014fe1ce6eada7a843b0309
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-10 21:43:15 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number N");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < number)
+{
+   if (i % 2 == 0)
+   {
+      Console.WriteLine(i);
+      i++;
+   }
+   else
+   {
+      i++;
+   }  
+}
