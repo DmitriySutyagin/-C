372cff49adfda846c0e3d61495aff113530f0e4d
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-12 18:40:46 +0300

    Доработка 8 задаи.

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -2,12 +2,12 @@
 Console.WriteLine("Enter number N");
 int number = Convert.ToInt32(Console.ReadLine());
 int i = 1;
-while (i < number)
+while (i <= number)
 {
    if (i % 2 == 0)
    {
       Console.WriteLine(i);
-      i++;
+      i+=2;
    }
    else
    {
