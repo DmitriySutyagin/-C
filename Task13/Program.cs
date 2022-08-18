3b92a68ad29df03556e4235df88007410d9ea70f
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-18 15:48:46 +0300

    Решение 13 задачи.

---
+++ Task13/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number");
+string? number = Console.ReadLine();
+Console.WriteLine($"Your number : {number}");
+int thrid = int.Parse(number [2].ToString());
+Console.WriteLine($"{thrid}");
