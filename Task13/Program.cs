c253b0af675d4f4f2d1b2c89110b20c11f364082
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-18 16:39:13 +0300

    Решение 15 задачи

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,3 +4,9 @@ string? number = Console.ReadLine();
 Console.WriteLine($"Your number : {number}");
 int thrid = int.Parse(number [2].ToString());
 Console.WriteLine($"{thrid}");
+int n = number.Length;
+int i = 3;
+if (n < i )
+{
+    Console.WriteLine("No third digit");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter number from 1 to 7");
+int number = Convert.ToInt32(Console.ReadLine());
+switch(number)
+{
+    case 1:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 2:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 3:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 4:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 5:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 6:
+        Console.WriteLine("Выходной");
+        break;
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня недели");
+        break;
+}
+
