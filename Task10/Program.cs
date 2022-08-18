2e369e7732487ecbb6c88a6aeab5e86bb928d6a6
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-18 15:52:10 +0300

    Решение 10 задачи.

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Enter a three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number > 99 && number < 1000)
+{
+    result = number % 100 / 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Enter a positive three-digit number");
+}
