8fa60fb7aa6debc409060eb5b6aa7ddc7ecf1355
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-20 16:30:43 +0300

    Решение 19 задачи

---
+++ Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+string?[] point = new string [5];
+Console.WriteLine("Enter five-digit number");
+point[0] = Convert.ToString(Console.Read());
+point[1] = Convert.ToString(Console.Read());
+point[2] = Convert.ToString(Console.Read());
+point[3] = Convert.ToString(Console.Read());
+point[4] = Convert.ToString(Console.Read());
+if(point[0] == point[4] && point[1] == point[3])
+{
+    Console.WriteLine("Number of polydrome");
+}
+else
+{
+    Console.WriteLine("The number is not a polydrome");
+}
