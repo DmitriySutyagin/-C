1bbc77e94e1b35dbddb3996262c7471e7e4b8e89
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-21 14:31:16 +0300

    Решение 23 задачи.

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number N :");
+int N = Convert.ToInt32(Console.ReadLine());
+N = Math.Abs(N);
+for(int i = 0;i <= N;i++)
+{
+    Console.WriteLine(i*i*i);
+}
