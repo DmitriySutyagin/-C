88d9a8ca0905e49d498cb9dc9c12010706a47cdc
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-30 15:54:44 +0300

    Доработка 34 задачи

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,35 +1,25 @@
 ﻿Console.Clear();
-
-int min = 99;
-int max = 1000;
-int [] point (int min, int max)
+int [] point (int size,int min, int max)
 {
     Random rand =new Random();
-    int count = 0;
-    int [] arr = new int [10]; 
+
+    int [] arr = new int [size]; 
     for(int i = 0;i < arr.Length;i++)
     {
         arr[i] = rand.Next(min,max);
-        if(arr[i] % 2 == 0) 
-        {
-            count++;
-            Console.WriteLine($"В даннои массиве {count} четных чисел");
-
-        }  
     }
     return arr;
 
 }
-Console.WriteLine('[' + string.Join(",",point(min,max)) + ']');
+int [] array = point(10,99,1000);
+int count = 0;
+for(int i = 0;i < array.Length;i++)
+{
+    if(array[i] % 2 == 0) 
+    {
+        count++;
 
-// int [] PrintArray(int [] point)
-// {
-//     for(int i = 0;i < point.Length;i++)
-//     {
-//         if(point[i] % 2 == 0)
-//         {
-//             i++;
-//         }
-//         Console.WriteLine($"В даннои массиве {i} четных чисел");
-//     }
-// }
+    }
+}
+Console.WriteLine($"В даннои массиве {count} четных чисел");
+Console.WriteLine('[' + string.Join(",",array) + ']');
