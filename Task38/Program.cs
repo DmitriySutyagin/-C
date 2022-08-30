0390d97e3ae68ba9afa24a7f7a474a2a53f0445b
Dmitriy <Sutyagin15086733@yandex.ru>
2022-08-30 22:59:13 +0300

    Решение 38 задачи.

---
+++ Task38/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+double[] FillArray(int size)
+{
+    double [] arr = new double [size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+       Console.WriteLine("Введите  число массива :");
+       double A = Convert.ToDouble(Console.ReadLine());
+       arr[i] = A;
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите размер число массива :");
+int size = Convert.ToInt32(Console.ReadLine());
+double sum = 0;
+double[] array = FillArray(size);
+for(int i = 0;i < array.Length;i++)
+{
+    sum = array[0] - array[array.Length - 1];
+    sum = Math.Abs(sum);
+}
+Console.WriteLine($"Разница чисел {sum}");
+Console.WriteLine('[' + string.Join(", ", array) + ']');
