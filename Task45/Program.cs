cdd45c3389e7a539af6e661817df31c3c6acb61f
Dmitriy <Sutyagin15086733@yandex.ru>
2022-09-03 15:57:09 +0300

    Решение 45 задачи

---
+++ Task45/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int[] point(int size)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1,10);
+    }  
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+     for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i];
+    }
+    return ;
+}
+Console.WriteLine("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = point(size);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+PrintArray(array);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
