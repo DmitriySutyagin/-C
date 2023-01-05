3893d6abf36ad06d029dbd01d7477a53108eec2f
Dmitrii <sutyagin15086733@yandex.ru>
2023-01-05 11:58:35 +0300

    Модернизация задачи

---
+++ Task45(dop)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N. 
+// (использовать рекурсию)
+// 4 -> 24
+// 5 -> 120
+Console.Clear();
+void PrintMulti(int N)
+{
+    
+    int count = -N;
+    if(0 <= -N) count--;
+    else return;
+    
+    PrintMulti(N * count);
+    Console.WriteLine(N * count);
+}
+
+Console.WriteLine("Enter numbers N:");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintMulti(number);
