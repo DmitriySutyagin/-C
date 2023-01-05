1c1131dc3c57889d60b2d14786c7f4092834c91f
Dmitrii <sutyagin15086733@yandex.ru>
2023-01-05 11:54:45 +0300

    Модернизация задачи

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -15,7 +15,7 @@ for (int i = 0; i < FirstMatrix.GetLength(0); i++)
 {
     for (int j = 0; j < FirstMatrix.GetLength(1); j++)
     {
-        FirstMatrix[i, j] = new Random().Next(0, 10);
+        FirstMatrix[i, j] = new Random().Next(1, 10);
         Console.Write(FirstMatrix[i, j] + " ");
     }
     Console.WriteLine();
@@ -27,10 +27,37 @@ int[,] SecondMatrix = new int[m, n];
     {
         for (int j = 0; j < SecondMatrix.GetLength(1); j++)
         {
-            SecondMatrix[i, j] = new Random().Next(0, 10);
+            SecondMatrix[i, j] = new Random().Next(1, 10);
             Console.Write(SecondMatrix[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
+void MulMatrix(int[,] SecondMatrix, int[,] FirstMatrix)
+{
+    int Rec = 0;
+    int[,] MultiMatrix = new int[SecondMatrix.GetLength(0), SecondMatrix.GetLength(1)];
+    // for (int k = 0; k < FirstMatrix.GetLength(0); k++)
+    {
+        for (int q = 0; q < SecondMatrix.GetLength(1); q++)
+        {
+            int Sum = 0;
 
+            for (int i = 0; i < SecondMatrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < SecondMatrix.GetLength(0); j++)
+                {
+                    Rec = FirstMatrix[i, j] * SecondMatrix[j, i];
+                }
+                    Sum = Sum + Rec;
+            }
+            Console.Write(Sum + " ");
+
+
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+}
+MulMatrix(SecondMatrix, FirstMatrix);
